db01a6965f0c46a8062d04ee44efdaeb372a3b40
Paweł Walczewski <paulwalczewski@icloud.com>
2021-04-01 13:29:01 +0100

    test update

--- pages/data-example.js
+++ pages/data-example.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from 'next/link';
 
 const ProductPage = ({ data }) => {
-  console.log('data', data);
+  useEffect(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+
+    console.log('data=', data);
+  }, [])
 
   return (
     <>
@@ -10,18 +15,9 @@ const ProductPage = ({ data }) => {
         <Link href="/">Home</Link>
       </div>
       <div>Data example </div>
-      <pre>{JSON.stringify(data, null, 4)}</pre>
+    
     </>
   )
-}
-
-export async function getStaticProps() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
-
-  return {
-    props: { data }
-  }
 }
 
 export default ProductPage;
